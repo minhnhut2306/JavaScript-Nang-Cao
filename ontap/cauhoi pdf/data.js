61c5c9d2b751efc2bff190d577d621a8afff80bf
unknown <nhutchodiennguyen23062004@gmail.com>
2024-02-26 16:28:09 +0700

    cau2

---
+++ ontap/cauhoi pdf/data.js
@@ -0,0 +1,9 @@
+let arr = ["t", "c", "e", "j", "b", "o", "-", "y", "a", "r", "r", "a"];
+let arr1 = [14, 12, 23, 43, 35, 30];
+let arr2 = [91, 82, 19, 73, 46, 20];
+let coursr = {
+    name : 'JS nâng cao',
+    id : 'wed2062',
+    lesson : 'Modern Operators'
+}
+
--- ontap/cauhoi pdf/ontap.js
+++ ontap/cauhoi pdf/ontap.js
@@ -69,3 +69,114 @@ var cau2b = document.getElementById("2.2");
 var numbers = arr3.filter((num) => num % 2 === 0 && (num < 20 || num > 70));
 cau2b.innerHTML = numbers.join(", ");
 console.log(numbers.length > 0 ? numbers.join(", ") : " ");
+
+console.log("2.3");
+var cau2c = document.getElementById("2.3");
+var isPrime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+var primeNumbers = [];
+for (let i = 30; i <= 80; i++) {
+  if (isPrime(i)) {
+    primeNumbers.push(i);
+    console.log(i);
+  }
+}
+cau2c.innerHTML = primeNumbers.join(", ");
+
+console.log("câu 2.4");
+var oddNumbers = arr3.filter((num) => num % 2 !== 0);
+oddNumbers.sort((a, b) => b - a);
+console.log(oddNumbers);
+var cau2d = document.getElementById("2.4");
+cau2d.innerHTML = oddNumbers.join(", ");
+
+console.log("câu 4");
+var lesson = coursr?.lesson ?? " lesson not available";
+console.log(lesson);
+
+// bài tập 2
+// câu 1
+const sortArray = (arr) => arr.sort((a, b) => a - b);
+const sortArray2 = (arr) =>
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+const sortArray3 = (arr, callback) => {
+  const sortArray4 = arr.sort((a, b) => a - b);
+  callback(sortArray4);
+};
+const displaysort = (arr2) => {
+  console.log("mảng được sắp xếp tăng dần ", arr2);
+};
+sortArray3(arr2, displaysort);
+
+// câu 2
+// Hàm nhập mảng từ người dùng
+const inputArray = (callback) => {
+  const userInput = prompt(
+    "Nhập mảng số tự nhiên (ít nhất 10 số), cách nhau bằng dấu phẩy:"
+  );
+  // Kiểm tra nếu userInput là null hoặc rỗng
+  if (userInput === null || userInput.trim() === "") {
+    console.log("Không có dữ liệu được nhập.");
+    return;
+  }
+  const userArray = userInput.split(",").map(Number);
+  callback(userArray);
+};
+
+// Hàm hiển thị mảng đã nhập
+const displayArray = (array) => {
+  console.log("Mảng đã nhập:", array);
+};
+
+// Hàm tìm số lớn nhất trong mảng và hiển thị ra màn hình
+const findMaxNumber = (array, callback) => {
+  const maxNumber = Math.max(...array);
+  callback(maxNumber);
+};
+
+// Hàm đảo ngược thứ tự các phần tử của mảng và hiển thị ra màn hình
+const Reverse = (array, callback) => {
+  const reversedArray = [...array].reverse();
+  callback(reversedArray);
+};
+
+// Hàm kiểm tra số có trong mảng hay không và hiển thị kết quả ra màn hình
+const checkNumberArray = (array, number, callback) => {
+  const isInArray = array.includes(number);
+  callback(isInArray);
+};
+
+const displayArrayResult = (isInArray) => {
+  if (isInArray) {
+    console.log("Số này đã tồn tại trong mảng");
+  } else {
+    console.log("Số này chưa tồn tại trong mảng");
+  }
+};
+
+// Lấy số nhập từ người dùng
+const userNumber = parseInt(prompt("Nhập một số bất kỳ:"));
+
+// Gọi hàm nhập mảng và sử dụng các callback function để thực hiện các nhiệm vụ cần thiết
+inputArray((userArray) => {
+  displayArray(userArray);
+  findMaxNumber(userArray, (maxNumber) => {
+    console.log("Số lớn nhất trong mảng:", maxNumber);
+  });
+  Reverse(userArray, (reversedArray) => {
+    console.log("Mảng đảo ngược là:", reversedArray);
+  });
+
+  if (!isNaN(userNumber)) {
+    checkNumberArray(userArray, userNumber, displayArrayResult);
+  } else {
+    console.log("Vui lòng nhập một số.");
+  }
+});
